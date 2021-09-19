12c8cc9f6df3ad275e3397b7e65a08b347e17327
hcha <hcha@node0.hcha-106407.uwmadison744-f21-pg0.wisc.cloudlab.us>
2021-09-19 08:39:03 -0500

    Add a basic experiment script

---
+++ assignment1/part3/task1/run.sh
@@ -0,0 +1,11 @@
+timestamp=`date "+%Y-%m-%d-%H%M%S"`
+echo -n "Enter the dataset name ('web-BerkStan'|'enwiki-pages-articles') and press [ENTER]:"
+read dataset_name
+
+spark-submit part2_simple.py $input_path 
+start_time="$(date -u +%s)"
+spark-submit pagerank.py $dataset_name
+end_time="$(date -u +%s)"
+elapsed="$(($end_time-$start_time))"
+echo $elapsed > "${dataset_name}_${timestamp}.log"
+echo "Job done! ${elapsed} seconds elapsed..."
