49ce902b0efd8f5ca8e71b5b9c6f252055ebc81a
hcha <hcha@node0.hcha-106407.uwmadison744-f21-pg0.wisc.cloudlab.us>
2021-09-19 09:15:56 -0500

    Add task2 basic codes

---
+++ assignment1/part3/task2/run.sh
@@ -0,0 +1,14 @@
+timestamp=`date "+%Y-%m-%d-%H%M%S"`
+echo -n "Enter the dataset name ('web-BerkStan'|'enwiki-pages-articles') and press [ENTER]:"
+read dataset_name
+echo -n "Enter the number of partitions and press [ENTER]:"
+read num_partitions
+
+start_time="$(date -u +%s)"
+spark-submit pagerank_partition.py $dataset_name $num_partitions
+end_time="$(date -u +%s)"
+elapsed="$(($end_time-$start_time))"
+
+echo $elapsed > "${dataset_name}_${num_partitions}_${timestamp}.log"
+
+echo "Job done! ${elapsed} seconds elapsed..."
