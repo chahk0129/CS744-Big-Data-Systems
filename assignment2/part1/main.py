d822b609b59d28ccc6bbf3d0eda4ee6fd125df9a
hcha <hcha@node0.g12.uwmadison744-f21-pg0.wisc.cloudlab.us>
2021-10-02 04:42:02 -0500

    Implement requirements (log loss every 20 iterations), add other options using argparser

--- assignment2/part1/main.py
+++ assignment2/part1/main.py
@@ -1,8 +1,10 @@
+import argparse
 import os
 import torch
 import json
 import copy
 import numpy as np
+import pandas as pd
 from torchvision import datasets, transforms
 import torch.nn as nn
 import torch.nn.functional as F
@@ -10,10 +12,13 @@ import torch.optim as optim
 import logging
 import random
 import model as mdl
+import time
 device = "cpu"
 torch.set_num_threads(4)
-
 batch_size = 256 # batch for one node
+log_iter = 20
+
+
 def train_model(model, train_loader, optimizer, criterion, epoch):
     """
     model (torch.nn.module): The model created to train
@@ -24,17 +29,26 @@ def train_model(model, train_loader, optimizer, criterion, epoch):
     """
 
     # remember to exit the train loop at end of the epoch
+    running_loss = 0
+    running_count = 0
     for batch_idx, (data, target) in enumerate(train_loader):
         # Reference: https://github.com/pytorch/examples/blob/master/mnist/main.py
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
         loss = criterion(output, target)
+        running_loss += loss.item()
+        running_count += batch_size
         loss.backward()
         optimizer.step()
-        print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+        if batch_idx % log_iter == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
+            # reset running loss, count
+            running_loss = 0
+            running_count = 0
+
     return None
 
 def test_model(model, test_loader, criterion):
@@ -56,6 +70,18 @@ def test_model(model, test_loader, criterion):
             
 
 def main():
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--master-ip', type=str, default='10.10.1.1', help='master node ip (default: 10.10.1.1)')
+    parser.add_argument('--num-nodes', type=int, default=4, help='the number of nodes (default:4)')
+    parser.add_argument('--rank', type=int, default=0, help='rank of node')
+    parser.add_argument('--epoch', type=int, default=1, help='the number of epochs (default:1)')
+    parser.add_argument('--exp_iter', type=int, default=10, help='the number of one epoch training (default:10)')
+    parser.add_argument('--output_path', type=str, default='elapsed_time.csv', help='output (elapsed time) path')
+    args = parser.parse_args()
+    epoch = args.epoch
+
+
     normalize = transforms.Normalize(mean=[x/255.0 for x in [125.3, 123.0, 113.9]],
                                 std=[x/255.0 for x in [63.0, 62.1, 66.7]])
     transform_train = transforms.Compose([
@@ -91,9 +117,17 @@ def main():
     optimizer = optim.SGD(model.parameters(), lr=0.1,
                           momentum=0.9, weight_decay=0.0001)
     # running training for one epoch
-    for epoch in range(1):
+    df = pd.DataFrame()
+    for exp_num in range(args.exp_iter):
+        start_time = time.time()
         train_model(model, train_loader, optimizer, training_criterion, epoch)
         test_model(model, test_loader, training_criterion)
+        elapsed_time = time.time() - start_time
+        df = df.append({
+            'exp_num': exp_num,
+            'elapsed_time': elasped_time
+        }, ignore_index=True)
+    df.to_csv(args.output_path)
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
     main()
