f93c981fc89ed17f16f29271207ce4fa12de5678
hcha <hcha@node0.g12.uwmadison744-f21-pg0.wisc.cloudlab.us>
2021-10-09 01:12:37 -0500

    added measuring network stats

--- assignment2/part2/taskB/run.sh
+++ assignment2/part2/taskB/run.sh
@@ -5,8 +5,7 @@ ip_addr=$(hostname -I)
 ip=(${ip_addr//" "/ })
 
 master_ip=10.10.1.1
-num_nodes="4"
-#num_nodes="1 2 3 4"
+num_nodes="1 2 3 4"
 
 ## directory info for all nodes
 node0=cs744/assignment2/part2/taskB
@@ -25,19 +24,83 @@ ssh node3 mkdir ${node3}/output
 
 for n in $num_nodes; do
 	if [[ "$n" = "1" ]]; then
+		## collects network stats of all nodes before execution
+		echo -e "---------- Network Stat. (Before) -------------" >> output/num${n}_rank0.stat
+		cat /proc/net/dev >> output/num${n}_rank0.stat
+
+		## app execution
 		python main.py --master-ip $master_ip --num-nodes $n --rank 0 >> output/num${n}_rank0.out
+
+		## collects network stats of all nodes after execution
+		echo -e "---------- Network Stat. (After) -------------" >> output/num${n}_rank0.stat
+		cat /proc/net/dev >> output/num${n}_rank0.stat
+
 	elif [[ "$n" = "2" ]]; then
+		## collects network stats of all nodes before execution
+		echo -e "---------- Network Stat. (Before) -------------" >> output/num${n}_rank0.stat
+		cat /proc/net/dev >> output/num${n}_rank0.stat
+		echo -e "---------- Network Stat. (Before) -------------" >> output/num${n}_rank1.stat
+		ssh node1 cat /proc/net/dev >> output/num${n}_rank1.stat
+
+		## app execution
 		python main.py --master-ip $master_ip --num-nodes $n --rank 0 >> output/num${n}_rank0.out &
 		ssh node1 "$export_cmd; cd ${node1}; python main.py --master-ip $master_ip --num-nodes $n --rank 1 >> output/num${n}_rank1.out"
+
+		## collects network stats of all nodes after execution
+		echo -e "---------- Network Stat. (After) -------------" >> output/num${n}_rank0.stat
+		cat /proc/net/dev >> output/num${n}_rank0.stat
+		echo -e "---------- Network Stat. (After) -------------" >> output/num${n}_rank1.stat
+		ssh node1 cat /proc/net/dev >> output/num${n}_rank1.stat
+
 	elif [[ "$n" = "3" ]]; then
+		## collects network stats of all nodes before execution
+		echo -e "---------- Network Stat. (Before) -------------" >> output/num${n}_rank0.stat
+		cat /proc/net/dev >> output/num${n}_rank0.stat
+		echo -e "---------- Network Stat. (Before) -------------" >> output/num${n}_rank1.stat
+		ssh node1 cat /proc/net/dev >> output/num${n}_rank1.stat
+		echo -e "---------- Network Stat. (Before) -------------" >> output/num${n}_rank2.stat
+		ssh node2 cat /proc/net/dev >> output/num${n}_rank2.stat
+
+		## app execution
 		python main.py --master-ip $master_ip --num-nodes $n --rank 0 >> output/num${n}_rank0.out &
 		ssh node1 "$export_cmd; cd ${node1}; python main.py --master-ip $master_ip --num-nodes $n --rank 1 >> output/num${n}_rank1.out" &
 		ssh node2 "$export_cmd; cd ${node2}; python main.py --master-ip $master_ip --num-nodes $n --rank 2 >> output/num${n}_rank2.out" 
+
+		## collects network stats of all nodes after execution
+		echo -e "---------- Network Stat. (After) -------------" >> output/num${n}_rank0.stat
+		cat /proc/net/dev >> output/num${n}_rank0.stat
+		echo -e "---------- Network Stat. (After) -------------" >> output/num${n}_rank1.stat
+		ssh node1 cat /proc/net/dev >> output/num${n}_rank1.stat
+		echo -e "---------- Network Stat. (After) -------------" >> output/num${n}_rank2.stat
+		ssh node2 cat /proc/net/dev >> output/num${n}_rank2.stat
+
 	elif [[ "$n" = "4" ]]; then
+		## collects network stats of all nodes before execution
+		echo -e "---------- Network Stat. (Before) -------------" >> output/num${n}_rank0.stat
+		cat /proc/net/dev >> output/num${n}_rank0.stat
+		echo -e "---------- Network Stat. (Before) -------------" >> output/num${n}_rank1.stat
+		ssh node1 cat /proc/net/dev >> output/num${n}_rank1.stat
+		echo -e "---------- Network Stat. (Before) -------------" >> output/num${n}_rank2.stat
+		ssh node2 cat /proc/net/dev >> output/num${n}_rank2.stat
+		echo -e "---------- Network Stat. (Before) -------------" >> output/num${n}_rank3.stat
+		ssh node3 cat /proc/net/dev >> output/num${n}_rank3.stat
+
+		## app execution
 		python main.py --master-ip $master_ip --num-nodes $n --rank 0 >> output/num${n}_rank0.out &
 		ssh node1 "$export_cmd; cd ${node1}; python main.py --master-ip $master_ip --num-nodes $n --rank 1 >> output/num${n}_rank1.out" &
 		ssh node2 "$export_cmd; cd ${node2}; python main.py --master-ip $master_ip --num-nodes $n --rank 2 >> output/num${n}_rank2.out" &
 		ssh node3 "$export_cmd; cd ${node3}; python main.py --master-ip $master_ip --num-nodes $n --rank 3 >> output/num${n}_rank3.out" 
+
+		## collects network stats of all nodes after execution
+		echo -e "---------- Network Stat. (After) -------------" >> output/num${n}_rank0.stat
+		cat /proc/net/dev >> output/num${n}_rank0.stat
+		echo -e "---------- Network Stat. (After) -------------" >> output/num${n}_rank1.stat
+		ssh node1 cat /proc/net/dev >> output/num${n}_rank1.stat
+		echo -e "---------- Network Stat. (After) -------------" >> output/num${n}_rank2.stat
+		ssh node2 cat /proc/net/dev >> output/num${n}_rank2.stat
+		echo -e "---------- Network Stat. (After) -------------" >> output/num${n}_rank3.stat
+		ssh node3 cat /proc/net/dev >> output/num${n}_rank3.stat
+
 	else
 		echo "Not supported number of nodes! (only supported up to 4)"
 		exit 1
