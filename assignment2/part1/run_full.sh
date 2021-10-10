2ec7da3d42fd7c055cdfc4038b1abb7522394a87
hcha <hcha@node0.g12.uwmadison744-f21-pg0.wisc.cloudlab.us>
2021-10-10 11:08:30 -0500

    Fix a bug

--- assignment2/part1/run_full.sh
+++ assignment2/part1/run_full.sh
@@ -4,6 +4,4 @@ ip=10.10.1.1
 
 mkdir -p output
 
-for ep in $epoch; do
-	python main.py --master-ip $ip --num-nodes 1 --rank 0 --epoch 25 --stop_iter 200 >> output/25_200.out
-done
+python main.py --master-ip $ip --num-nodes 1 --rank 0 --epoch 25 --stop_iter 200 >> output/25_200.out
