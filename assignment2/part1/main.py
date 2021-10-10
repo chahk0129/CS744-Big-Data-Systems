af748b4ed85684473c73aaaf785c3961d6b14b46
hcha <hcha@node0.g12.uwmadison744-f21-pg0.wisc.cloudlab.us>
2021-10-09 22:29:51 -0500

    Finalize part1

--- assignment2/part1/main.py
+++ assignment2/part1/main.py
@@ -17,34 +17,41 @@ torch.set_num_threads(4)
 batch_size = 256 # batch for one node
 log_iter = 20
 
-
-def train_model(model, train_loader, optimizer, criterion, epoch):
+def train_model(model, train_loader, optimizer, criterion, epoch, stop_iter):
     """
     model (torch.nn.module): The model created to train
     train_loader (pytorch data loader): Training data loader
     optimizer (optimizer.*): A instance of some sort of optimizer, usually SGD
     criterion (nn.CrossEntropyLoss) : Loss function used to train the network
     epoch (int): Current epoch number
+    stop_iter (int): Stop iteration at the predetermined iteration
     """
     start_time = time.time()
     log_iter_start_time = time.time()
-    for batch_idx, (data, target) in enumerate(train_loader):
-        # Reference: https://github.com/pytorch/examples/blob/master/mnist/main.py
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
-        output = model(data)
-        loss = criterion(output, target)
-        loss.backward()
-        optimizer.step()
+    with open(f'output/{log_file_name}', 'a+') as f:
+        for batch_idx, (data, target) in enumerate(train_loader):
+            if batch_idx >= stop_iter:
+                break
+            batch_count = batch_idx + 1
+            # Reference: https://github.com/pytorch/examples/blob/master/mnist/main.py
+            data, target = data.to(device), target.to(device)
+            optimizer.zero_grad()
+            output = model(data)
+            loss = criterion(output, target)
+            loss.backward()
+            optimizer.step()
 
-        elapsed_time = time.time() - start_time
-        start_time = time.time()
-        if batch_idx % log_iter == 0:
-            log_iter_elpased_time = time.time() - start_time
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\t elapsed time: {:.3f}'.format(
-                epoch, batch_idx * len(data) * group_size, len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.item(), log_iter_elapsed_time)) 
-            log_iter_start_time = time.time()
+            elapsed_time = time.time() - start_time
+
+            f.write(f"{epoch},{batch_count},{elapsed_time}\n")
+
+            start_time = time.time()
+            if batch_count % log_iter == 0:
+                log_iter_elapsed_time = time.time() - log_iter_start_time
+                print('Train Epoch: {} \t Iteration: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\t elapsed time: {:.3f}'.format(
+                    epoch, batch_count, batch_count * len(data), len(train_loader.dataset),
+                    100. * batch_count / len(train_loader), loss.item(), log_iter_elapsed_time)) 
+                log_iter_start_time = time.time()
     return None
 
 def test_model(model, test_loader, criterion):
@@ -72,10 +79,15 @@ def main():
     parser.add_argument('--num-nodes', type=int, default=1, help='the number of nodes (default:1)')
     parser.add_argument('--rank', type=int, default=0, help='rank of node')
     parser.add_argument('--epoch', type=int, default=1, help='the number of epochs (default:1)')
-    parser.add_argument('--exp_iter', type=int, default=10, help='the number of one epoch training (default:10)')
-    parser.add_argument('--output_path', type=str, default='elapsed_time.csv', help='output (elapsed time) path')
+    parser.add_argument('--stop_iter', type=int, default=40, help='Stop iteration at, (default: 40)')
+
     args = parser.parse_args()
-    epoch = args.epoch
+    num_epochs = args.epoch
+    stop_iter = args.stop_iter
+    global log_file_name
+    log_file_name = f"timelog_{num_epochs}_{stop_iter}.csv"
+    with open(f'output/{log_file_name}', 'w+') as f:
+        f.write("epoch,iteration,elpased_time\n")
 
     normalize = transforms.Normalize(mean=[x/255.0 for x in [125.3, 123.0, 113.9]],
                                 std=[x/255.0 for x in [63.0, 62.1, 66.7]])
@@ -111,6 +123,9 @@ def main():
     model.to(device)
     optimizer = optim.SGD(model.parameters(), lr=0.1,
                           momentum=0.9, weight_decay=0.0001)
+    for epoch in range(num_epochs):
+        train_model(model, train_loader, optimizer, training_criterion, epoch, stop_iter)
+    test_model(model, test_loader, training_criterion)
 
 if __name__ == "__main__": 
     main()
--- assignment2/part1/run.sh
+++ assignment2/part1/run.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 ip=10.10.1.1
-iter=(2 4 6 8 10)
 
 mkdir -p output
 
-for it in $iter; do
-	python main.py --master-ip $ip --num-nodes 1 --rank 0 --exp_iter $it >> output/${it}.out
-done
+python main.py --master-ip $ip --num-nodes 1 --rank 0  >> output/1_40.out
