0a43c792de6a8fef1813b295300b96635865fd98
hcha <hcha@node0.g12.uwmadison744-f21-pg0.wisc.cloudlab.us>
2021-10-10 00:51:10 -0500

    Last experiment (hopefully...)

---
+++ assignment2/run_full_all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cur=$(PWD)
+
+## run part1
+cd part1
+./run_full.sh
+
+## run part2
+cd $cur/part2/taskA
+./run_full.sh
+cd $cur/part2/taskB
+./run_full.sh
+
+## run part3
+cd $cur/part3
+./run_full.sh
